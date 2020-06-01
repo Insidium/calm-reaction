35f34690f358fca5af044688fac603a1584e590c
Insidium <ryanchallen@outlook.com>
2020-06-01 17:17:58 +1000

    Removed React boilerplate and added App class. Added states for audio playback, nightmode and meditation focus. Bound this to toggleAudio function. Set url for current audio track. Added logic for toggling state of playback, true and false. Added button for playback and applied onClick

--- calm-reaction/calm-reaction/src/App.js
+++ calm-reaction/calm-reaction/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isAudioPlaying: false,
+			isNightMode: false,
+			meditationFocus: 'Set meditation focus',
+		};
+		this.toggleAudio = this.toggleAudio.bind(this);
+		this.url = 'https://www.bensound.com/bensound-music/bensound-relaxing.mp3';
+		this.audio = new Audio(this.url);
+	}
+	toggleAudio() {
+		if (this.state.isAudioPlaying === false) {
+			this.setState({
+				isAudioPlaying: true,
+			});
+			this.audio.play();
+		} else {
+			this.setState({
+				isAudioPlaying: false,
+			});
+			this.audio.pause();
+		}
+		console.log(this.state.isAudioPlaying);
+	}
+	render() {
+		return (
+			<div className='container'>
+				<button onClick={this.toggleAudio}>Play</button>
+			</div>
+		);
+	}
 }
 
 export default App;
