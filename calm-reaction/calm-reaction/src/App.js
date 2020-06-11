bf785c94b8f7440eb9bff83e4e55319ec37d5821
Insidium <ryanchallen@outlook.com>
2020-06-11 17:04:19 +1000

    Added toggleNightMode functionality with moon and sun emoji on checkbox for now, added initial background styling for body and root id for both day (gradient with animations) and nightmodes (dark colour static)

--- calm-reaction/calm-reaction/src/App.js
+++ calm-reaction/calm-reaction/src/App.js
@@ -12,10 +12,11 @@ class App extends Component {
 		this.state = {
 			isAudioPlaying: false,
 			isNightMode: false,
-			meditationFocus: 'Set meditation focus',
+			meditationFocus: 'Set meditation focus'
 		};
 		// bind the this keyword for use on toggleAudio
 		this.toggleAudio = this.toggleAudio.bind(this);
+		this.toggleNightMode = this.toggleNightMode.bind(this);
 		// define url for audio source
 		this.url = 'https://www.bensound.com/bensound-music/bensound-relaxing.mp3';
 		// define new audio playback from the url above
@@ -26,26 +27,51 @@ class App extends Component {
 		// if playback is off/paused, change to on
 		if (this.state.isAudioPlaying === false) {
 			this.setState({
-				isAudioPlaying: true,
+				isAudioPlaying: true
 			});
 			// play audio track
 			this.audio.play();
 			// else if playback is on, change to off
 		} else {
 			this.setState({
-				isAudioPlaying: false,
+				isAudioPlaying: false
 			});
 			// pause audio track
 			this.audio.pause();
 		}
 		// console.log(this.state.isAudioPlaying); // test for audio toggle
 	}
+
+	toggleNightMode() {
+		// if (this.state.isNightMode === false) {
+		// 	this.setState({
+		// 		isNightMode: true
+		// 	});
+		// } else {
+		// 	this.setState({
+		// 		isNightMode: false
+		// 	});
+		// }
+		// easy toggle!
+		this.setState({ isNightMode: !this.state.isNightMode });
+	}
 	// app render for display in browser
 	render() {
+		const isNightMode = this.state.isNightMode;
+		const nightModeMessage = isNightMode ? '🌙' : '☀️';
 		return (
 			// main app container
-			<div className='container'>
+			<div className={isNightMode ? `container nightmode` : `container`}>
 				{/* button taking on toggleAudio click event for play/pause */}
+				<input
+					onClick={this.toggleNightMode}
+					type='checkbox'
+					id='nightmode'
+					checked={isNightMode}
+				/>
+				<div className='nightmoji'>
+					<p>{nightModeMessage}</p>
+				</div>
 				<button onClick={this.toggleAudio}>Play</button>
 			</div>
 		);
